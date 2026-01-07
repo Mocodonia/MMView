b587902dd867088124053256fc66f90111cabadb
7Zeb <66693249+7zeb@users.noreply.github.com>
2026-01-07 13:26:17 -0600

    Update place name in viewer from current location
    Add functionality to update place name from JSON.

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const button = document.getElementById('testButton'); //declare the testing thin
 button.addEventListener('click', () => {
   alert('MMView is online.');
 });
+
+//for viewer.html later to automatically update the name of the place, will have to pull from a JSON file with the image name
+document.getElementById("placeName").textContent = currentLocationName;
